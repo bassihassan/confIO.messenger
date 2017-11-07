84333db3f2d757adfee40a7c7bd95f2fc94b8f1a
Amine Hakkou <amine.hakkou.pro@gmail.com>
2017-11-07 12:23:43 +0000

    Hello world using vertx web

--- src/main/kotlin/io/xhub/confIOMessenger/Main.kt
+++ src/main/kotlin/io/xhub/confIOMessenger/Main.kt
@@ -2,11 +2,9 @@ package io.xhub.confIOMessenger
 
 import io.vertx.core.Launcher
 
-object Main {
-    @JvmStatic fun main(args: Array<String>) {
-        val argList = mutableListOf("run", "io.xhub.confIOMessenger.RootVerticle")
-        argList.addAll(args)
+fun main(args: Array<String>) {
+    val argList = mutableListOf("run", "io.xhub.confIOMessenger.RootVerticle")
+    argList.addAll(args)
 
-        Launcher().dispatch(argList.toTypedArray())
-    }
+    Launcher().dispatch(argList.toTypedArray())
 }
--- src/main/kotlin/io/xhub/confIOMessenger/RootVerticle.kt
+++ src/main/kotlin/io/xhub/confIOMessenger/RootVerticle.kt
@@ -1,14 +1,23 @@
 package io.xhub.confIOMessenger
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.http.HttpServer
 
 class RootVerticle: AbstractVerticle() {
+    var server: HttpServer? = null
 
     override fun start() {
-        super.start()
+        server = vertx.createHttpServer()
+        server?.requestHandler({ request ->
+            val response = request.response()
+            response.end("Hello world!")
+        })
+
+        server?.listen(8080)
+
     }
 
     override fun stop() {
-        super.stop()
+        server?.close()
     }
 }
